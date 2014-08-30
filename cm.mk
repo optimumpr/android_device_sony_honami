38f2acaf3bbeafbbf77ee232cc516c24887084ed
David Viteri <davidteri91@gmail.com>
2014-08-30 20:46:34 +0200

    Update cm.mk
    Follow Releases changes from cm

--- cm.mk
+++ cm.mk
@@ -1,6 +1,8 @@
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
+CM_BUILDTYPE := RELEASE
+
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
